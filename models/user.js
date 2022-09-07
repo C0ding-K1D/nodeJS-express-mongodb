b3105fe6742099350985f6aee8138534faa38204
Matthew Alvarez <you@example.com>
2022-09-07 14:05:38 -0400

    passport authenticate issues

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
